d209c0094571e347b254397c3309aa4cb268dc14
Zachary Chetchavat <zachary.chetchavat@gmail.com>
2022-09-11 17:05:04 -0500

    update assets with ops code

--- week_4/project/week_4.py
+++ week_4/project/week_4.py
@@ -5,22 +5,41 @@ from project.resources import redis_resource, s3_resource
 from project.types import Aggregation, Stock
 
 
-@asset
-def get_s3_data():
-    # Use your op logic from week 3
-    pass
+@asset(
+    required_resource_keys={"s3"},
+    config_schema={"s3_key": str},
+    out={"stocks": Out(dagster_type=List[Stock])},
+    description='List of Stocks'
+)
+def get_s3_data(context):
+    output = list()
+    s3_key = context.op_config['s3_key']
+    s3_data = context.resources.s3.get_data(s3_key)
+    for row in s3_data:
+        stock = Stock.from_list(row)
+        output.append(stock)
+    return output
 
 
-@asset
-def process_data():
-    # Use your op logic from week 3 (you will need to make a slight change)
-    pass
+@asset(
+    ins={"stocks": In(dagster_type=List[Stock])},
+    out={"agg": Out(dagster_type=Aggregation)},
+    description="Determine the Stock with the greatest high value",
+)
+def process_data(stocks):
+    sorted_stocks = sorted(stocks, key=lambda x: x.high, reverse=True) 
+    high_day = sorted_stocks[0].date
+    high_high = sorted_stocks[0].high
+    output = Aggregation(date=high_day, high=high_high)
+    return output
 
 
-@asset
-def put_redis_data():
-    # Use your op logic from week 3 (you will need to make a slight change)
-    pass
+@asset(
+    required_resource_keys={'redis'},
+    ins={"agg": In(dagster_type=Aggregation)}
+)
+def put_redis_data(context, agg):
+    context.resources.redis.put_data(str(agg.date), str(agg.high))
 
 
 get_s3_data_docker, process_data_docker, put_redis_data_docker = with_resources()
